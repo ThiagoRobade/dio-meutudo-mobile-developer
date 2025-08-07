16abff7d4c41563ec3f706529a5008bb9a234037
ThiagoRobade <robade@live.com>
2025-08-07 11:06:45 -0300

    feat: adiciona função de simulação de corrida entre dois personagens

--- 02-simulador-de-corridas-do-mario-kart-com-nodejs/src/index.js
+++ 02-simulador-de-corridas-do-mario-kart-com-nodejs/src/index.js
@@ -19,3 +19,18 @@ const player2 = {
 function rollDice(){
     return Math.floor(Math.random() * 6) + 1;
 }
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++) {
+        console.log(`Rodada ${round}`);
+    }
+}
+
+(async function main() {
+    console.log("" +
+        `🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...
+`
+    );
+    await playRaceEngine(player1, player2);
+})();
+
