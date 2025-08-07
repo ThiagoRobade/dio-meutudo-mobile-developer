825e93eed9f4858e3580594fe417f76a2cc26cc1
ThiagoRobade <robade@live.com>
2025-08-07 09:05:57 -0300

    feat: adiciona dois personagens e funcao rollDice

---
+++ 02-simulador-de-corridas-do-mario-kart-com-nodejs/src/index.js
@@ -0,0 +1,21 @@
+// Player1 - Mario
+const player1 = {
+    NOME: "Mario",
+    VELOCIDADE: 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+// Player2 - Luigi
+const player2 = {
+    NOME: "Luigi",
+    VELOCIDADE: 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
+
+function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+}
